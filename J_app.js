e37259b4b929b06d683f8423e6f51ca2488110f4
JCruz6725 <35982889+JCruz6725@users.noreply.github.com>
2023-04-11 15:58:52 -0500

    app upload

---
+++ J_app.js
@@ -0,0 +1,19 @@
+var http = require('http'); // Import Node.js core module
+
+var server = http.createServer(function (req, res) {   //create web server
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><h1>This is home Page.</h1></body></html>');
+        res.end();
+    
+    }
+
+    else
+        res.end('Invalid Request!');
+
+});
+server.listen(5000); //6 - listen for any incoming requests
